2014604c3d0e370c97efb22924a3703b46c88267
iceice54 <125724815+iceice54@users.noreply.github.com>
2025-12-29 02:54:02 +0800

    feat(api): add search route

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Query, Depends
+from backend.schemas import FlightSearch, FlightResult
+from backend.dependencies import FlightService, get_flight_service
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message" : "Hello"}
+
+@app.get("/search")
+async def search_flights(
+    search_query: Annotated[FlightSearch, Query()],
+    service: Annotated[FlightService, Depends(get_flight_service)]
+) -> list[FlightResult]:
+    results = service.get_flights(search_query)
+    return results
+    
